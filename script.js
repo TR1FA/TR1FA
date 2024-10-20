41aaac52e26254a59234aa679bcf3067e435d16a
TR1FA <tr1fa@outlook.com>
2024-10-20 06:38:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let isLoggedIn = false;
 const adminUsername = "admin";
-const adminPassword = "12345";
+const adminPassword = "123456";
+
+function login() {
+    const username = document.getElementById('admin-username').value;
+    const password = document.getElementById('admin-password').value;
+
+    if (username === adminUsername && password === adminPassword) {
+        isLoggedIn = true;
+        document.getElementById('login-error').style.display = 'none';
+        document.getElementById('score-update').style.display = 'block'; // Show score update section
+    } else {
+        document.getElementById('login-error').style.display = 'block';
+    }
+}
 
 // Podaci o igračima i njihovim bodovima
 const players = {
